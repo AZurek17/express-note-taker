fa3a74f2334a1486a588f32279d925cab93b8e2e
Andy <84168944+AZurek17@users.noreply.github.com>
2023-10-25 20:18:55 -0700

    added uuid helper

--- server.js
+++ server.js
@@ -1,29 +1,58 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const notesDb = require('./db/db.json');
 const PORT = 3000;
 
 const app = express();
 
+//generating unique ids
+const uuid = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+};
+
+//Middleware 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-
-
-//Routes
-app.get("/", function(req, res) {
+//GET Routes
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"))
 });
 
-app.get("/notes", function(req, res){
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"))
 })
 
-app.get("/api/notes", function(req, res){
-    return res.json(notesDb);})
+app.get("/api/notes", (req, res) => {res.json(notesDb);})
+
+//POST Routes
+app.post("/api/notes", (req, res) => {
+    console.info(`${req.method} request recieved to add a note`);
+
+    const { title, text } = req.body;
+    if (req.body) {
+        const newNote ={
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+   const noteString = JSON.stringify(newNote);
+    
+   fs.writeFile('./db/db.json', noteString, (err) =>
+    err ? console.error(err) : console.log(' Data written to db.json ') )
+    }
+}); 
+  
+   
 
     
+
 // 
 // create note - takes in JSON input
 // add.post("/api/notes/title" ,)
